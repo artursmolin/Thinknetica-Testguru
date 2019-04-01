ce2c723c306fbed44b96ab3dd35b3ae32b643ece
Artur <artursmolin@gmail.com>
2019-04-01 14:57:01 +0300

    fix

--- test_guru/db/migrate/20190401113030_add_answers_question_id_null_constrait.rb
+++ test_guru/db/migrate/20190401113030_add_answers_question_id_null_constrait.rb
@@ -4,11 +4,9 @@ class AddAnswersQuestionIdNullConstrait < ActiveRecord::Migration[5.2]
 
   def up
     change_column_null(:answers, :question_id, false)
-    change_column_default :correct, default: false
   end
 
   def down
     change_column_null(:answers, :question_id, true)
-    change_column_default :level, default: nil
   end
 end
--- test_guru/db/migrate/20190401113054_add_tests_category_id_null_constrait.rb
+++ test_guru/db/migrate/20190401113054_add_tests_category_id_null_constrait.rb
@@ -4,11 +4,9 @@ class AddTestsCategoryIdNullConstrait < ActiveRecord::Migration[5.2]
 
   def up
     change_column_null(:tests, :category_id, false)
-    change_column_default :level, default: 'Junior'
   end
 
   def down
     change_column_null(:tests, :category_id, true)
-    change_column_default :level, default: nil
   end
 end
