213ff4eba8d2eafdd5416bacd9a4f0bba6ea1ea5
Artur <artursmolin@gmail.com>
2019-04-04 01:34:19 +0300

    2,3 задание

--- test_guru/app/models/test.rb
+++ test_guru/app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  def self.by_category(category_name)
+    Test.joins("INNER JOIN categories ON tests.category_id = categories.id").
+    where(categories: {title: category_name}).
+    order(title: :desc).pluck(:title)
+  end
 end
--- test_guru/app/models/user.rb
+++ test_guru/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def test_level(user_id, level)
+    Test.joins("INNER JOIN user_tests ON tests.id = user_tests.test_id").
+    where(tests: {user_id: user_id, level: level})
+  end
 end
