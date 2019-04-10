120b5618674eee8b3781b68c6805cb89c1585dc2
Artur <artursmolin@gmail.com>
2019-04-10 20:39:52 +0300

    Update user.rb

--- test_guru/app/models/user.rb
+++ test_guru/app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   has_many :tests, through: :user_tests
   has_many :author_tests, class_name: "Test", foreign_key: "author_id"
 
-
-
   def test_level(level)
     tests.where(level: level)
   end
