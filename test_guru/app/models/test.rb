1486ceef10a3dfe3045c05a6017a5ec93c02d77b
Artur <artursmolin@gmail.com>
2019-04-11 22:00:08 +0300

    fix связи

--- test_guru/app/models/test.rb
+++ test_guru/app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :author, class_name: 'User', foreign_key: :user_id, optional: true
+  belongs_to :author, class_name: 'User'
   has_many :questions, dependent: :destroy
   has_many :user_tests
   has_many :users, through: :user_tests, dependent: :destroy
--- test_guru/db/migrate/20190410144023_add_author_to_test.rb
+++
@@ -1,5 +0,0 @@
-class AddAuthorToTest < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :tests, :author, foreign_key: {to_table: :users}
-  end
-end
