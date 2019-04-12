ebf5bd72eba8c1eb176c289713645d8c4b827422
Artur <artursmolin@gmail.com>
2019-04-12 09:17:56 +0300

    fix
    добавил потерянный метод
    добавил указание внешнего ключа на таблицу

--- test_guru/app/models/test.rb
+++ test_guru/app/models/test.rb
@@ -4,4 +4,10 @@ class Test < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :user_tests
   has_many :users, through: :user_tests, dependent: :destroy
+
+  def self.by_category(category_name)
+   joins(:category).
+     where(categories: { title: category_name }).
+       order(title: :desc).pluck(:title)
+  end
 end
