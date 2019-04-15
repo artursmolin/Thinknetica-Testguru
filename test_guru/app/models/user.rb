08b83f6d1fee5214d101f52e57caf9065c02afdd
Artur <artursmolin@gmail.com>
2019-04-16 00:48:01 +0300

    fix after lesson6

--- test_guru/app/models/user.rb
+++ test_guru/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :tests, through: :user_tests
   has_many :author_tests, class_name: "Test", foreign_key: "author_id"
 
-  validates :email, presence:true, uniqueness:true
+  validates :email, presence: true, uniqueness: true
 
   def test_level(level)
     tests.where(level: level)
