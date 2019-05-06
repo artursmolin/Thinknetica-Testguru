985a2f59f857246b9b2a54612139df9e21599d5e
Artur <artursmolin@gmail.com>
2019-05-06 11:07:04 +0300

    fix repo helper
    fix year + github repo

--- test_guru/app/helpers/application_helper.rb
+++ test_guru/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def today
+     Date.today.year
+  end
+
+  def github_url(author, repo = '')
+    text = repo == "" ? author : repo
+    link_to text, "https://github.com/#{author}/#{repo}", target: '_blank'
+  end
 end
