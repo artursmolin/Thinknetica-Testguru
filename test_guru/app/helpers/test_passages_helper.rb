41c1dc94c3b40c18a29a253c36d47cb9e764537e
Artur <artursmolin@gmail.com>
2019-05-12 20:33:03 +0300

    fix

--- test_guru/app/helpers/test_passages_helper.rb
+++ test_guru/app/helpers/test_passages_helper.rb
@@ -1,2 +1,8 @@
 module TestPassagesHelper
+
+  def result_test
+      question_size = @test_passage.test.questions.size.to_f
+      correct_size =  @test_passage.correct_question.to_f
+      result = ((correct_size / question_size))*100
+  end
 end
--- test_guru/app/models/test_passage.rb
+++ test_guru/app/models/test_passage.rb
@@ -3,7 +3,7 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_validation :set_first_question, on: :create
 
   before_save :next_question, on: :update
 
@@ -22,9 +22,18 @@ class TestPassage < ApplicationRecord
     current_question.answers.right
   end
 
+  def current_question_index
+    index = 1
+    if self.current_question.nil?
+      index = 1
+    else
+      index +=1
+    end
+  end
+
   private
 
-  def before_validation_set_first_question
+  def set_first_question
     self.current_question = test.questions.first if test.present?
   end
 
@@ -32,9 +41,9 @@ class TestPassage < ApplicationRecord
     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
-
-
   def next_question
     self.current_question = test.questions.order(:id).where('id >?', current_question.id).first
   end
+
+
 end
